f8ac55d3424bebdb1f2c31a2f2c654f2937c288d
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-04-11 21:02:48 +0200

    Move Request class to separate file

--- lib/rollapi.dart
+++ lib/rollapi.dart
@@ -5,34 +5,15 @@ import 'dart:core';
 import 'package:http/http.dart' as http;
 
 import 'src/exceptions.dart';
+import 'src/request.dart';
 import 'src/state.dart';
 
 export 'src/exceptions.dart';
+export 'src/request.dart';
 export 'src/state.dart';
 
 /// Base URL where API lives - must end with the '/'
 String API_BASE_URL = 'https://roll.lastgimbus.com/api/';
-
-/// Class representing a single request. All properties are immutable, and
-/// new states are emitted by [stateStream]
-class Request {
-  /// UUID of the request - can be used to fetch images etc
-  final String uuid;
-
-  /// Stream that emits new states
-  /// Possible values:
-  /// - RequestState.queued, DateTime eta
-  /// - RequestState.running, DateTime eta
-  /// - RequestState.finished, int result
-  /// - RequestState.expired and RequestState.failed, Exception e - can be:
-  ///
-  ///     - ApiException - generic, something just went wrong, try again
-  ///
-  ///     - ApiUnavailableException - api unavailable, probably maintenance
-  final Stream<MapEntry<RequestState, dynamic>> stateStream;
-
-  Request(this.uuid, this.stateStream);
-}
 
 /// Loop that checks for new states and handles the logic
 Stream<MapEntry<RequestState, dynamic>> _stateStream(String uuid) async* {
